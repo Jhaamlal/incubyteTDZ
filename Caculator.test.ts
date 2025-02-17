e54516b53071b934f99714f2115c4277fec58b6f
Jhaamlal <shankarji1729@gmail.com>
2025-02-17 12:19:59 +0530

    update with Basic setUp

---
+++ Caculator.test.ts
@@ -0,0 +1,10 @@
+/**
+ * Calculator => Take sting , and number
+ * Now we need to see How  Base case,normal Case,Edge Case ,Special Case
+ * what if both are empty
+ * what if one of them is empty
+ * If Something UN-CHANGE
+ */
+import { StringCalculator } from "./Calculator"
+
+describe("Calculator-test", () => {})
